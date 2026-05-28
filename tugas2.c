c2a5f29e55ad351b1e6c3aaae98ba4090f727052
cza <cutzahieraa@gmail.com>
2026-05-28 09:43:32 +0700

    Implement advanced sorting options including Quick Sort

--- tugas2.c
+++ tugas2.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 #define SIZE 1000
+#define MAX_WORDS 5000
+#define MAX_LEN 50
+
+char dataset[MAX_WORDS][MAX_LEN];
+int word_count = 0;
 
 // Function prototype
 void generateRandom(int arr[]);
 void displayData(int arr[]);
 void bubbleSort(int arr[]);
 void insertionSort(int arr[]);
+
+void bacaFile();
+void shuffleDataset();
+void displayDataset();
+
+void quickSort(int low, int high);
+int partition(int low, int high);
 
 int main() {
     int pilihan, metode;
@@ -85,8 +98,70 @@ int main() {
                 break;
 
             case 2:
-                printf("\nAdvance Sorting dikerjakan anggota lain.\n");
-                break;
+               do {
+                //submenu advance sorting
+                printf("\n===== ADVANCE SORTING =====\n");
+                printf("1. Merge Sort\n");
+                printf("2. Quick Sort\n");
+                printf("3. Shell Sort\n");
+                printf("4. Kembali\n");
+                printf("Pilih metode : ");
+                scanf("%d", &metode);
+
+                switch(metode) {
+                    case 1:
+                        printf("\nMerge Sort dikerjakan oleh orang 2.\n");
+                        break;
+                    case 2:
+                        //baca data dari words.txt
+                        printf("\nMembaca file dataset...\n");
+                        bacaFile();
+
+                        if (word_count == 0) {
+                            break;
+                        }
+
+                        //melakukan shuffle sebelum sorting
+                        printf("Melakukan shuffle dataset...\n");
+                        shuffleDataset();
+
+                        //menampilkan data sebelum sorting
+                        printf("\nData sebelum sorting: \n");
+                        displayDataset();
+
+                        //hitung waktu eksekusi quick sort
+                        clock_t start, end;
+                        double waktu;
+
+                        start = clock();
+
+                        quickSort(0, word_count - 1);
+
+                        end = clock();
+
+                        //menampilkan data setelah sorting
+                        printf("\nData setelah Quick Sort: \n");
+                        displayDataset();
+
+                        //menampilkan total waktu proses
+                        waktu = ((double)(end - start)) / CLOCKS_PER_SEC;
+                        printf("\nWaktu eksekusi Quick Sort : %f detik\n", waktu);
+                        break;
+
+                    case 3:
+                        printf("\nShell Sort dikerjakan oleh orang 2.\n");
+                        break;
+
+                     case 4:
+                         printf("\nKembali ke menu utama...\n");
+                         break;
+
+                    default:
+                        printf("\nPilihan tidak valid!\n");
+                }
+               }
+               while(metode != 4);
+               break;
 
             case 3:
                 printf("\nProgram selesai.\n");
@@ -96,7 +171,7 @@ int main() {
                 printf("\nPilihan tidak valid!\n");
         }
 
-    } while(pilihan != 3);
+    } while(pilihan != 4);
 
     return 0;
 }
@@ -174,4 +249,85 @@ void insertionSort(int arr[]) {
     waktu = ((double)(end - start)) / CLOCKS_PER_SEC;
 
     printf("\nWaktu eksekusi Insertion Sort : %f detik\n", waktu);
+}
+
+//membaca dataset dari words.txt
+void bacaFile(){
+    FILE *file = fopen("words.txt", "r");
+    word_count = 0;
+
+    if (file == NULL) {
+        printf("\nGagal membuka file words.txt!\n");
+        return;
+    }
+
+    while (fscanf(file, "%s", dataset[word_count]) != EOF && word_count < MAX_WORDS) {
+        word_count ++;
+    }
+
+    fclose(file);
+}
+
+//fungsi untuk menampilkan 10 kata pertama
+void displayDataset(){
+    if (word_count == 0) {
+        printf("Dataset kosong atau belum dibaca.\n");
+        return;
+    }
+
+    //menampilkan max 10 kata pertama
+    for (int i = 0; i < 10 && i < word_count; i++) {
+        printf("%s ", dataset[i]);
+    } 
+    printf("\n");
+}
+
+//fungsi untuk mengacak data set
+void shuffleDataset(){
+    char temp[MAX_LEN];
+    
+    for (int i = word_count - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+
+        strcpy(temp, dataset[i]);
+        strcpy(dataset[i], dataset[j]);
+        strcpy(dataset[j], temp);
+    }
+}
+
+//fungsi membantu quick sort membagi array berdasarkan pivot
+int partition(int low, int high) {
+    char pivot[MAX_LEN];
+    strcpy(pivot, dataset[high]);
+
+    int i = (low - 1);
+    char temp[MAX_LEN];
+
+    for (int j = low; j < high; j++) {
+        //strcmp(a, b) < 0 artinya kata 'a' < 'b'
+        if (strcmp(dataset[j], pivot) < 0) {
+            i++;
+
+             strcpy(temp, dataset[i]);
+             strcpy(dataset[i], dataset[j]);
+             strcpy(dataset[j], temp);
+        }
+    }
+    //tukar posisi pivot ke tempat yang benar
+    strcpy(temp, dataset[i + 1]);
+    strcpy(dataset[i + 1], dataset[high]);
+    strcpy(dataset[high], temp);
+
+    return (i  + 1);
+}
+
+//fungsi utama quick sort 
+void quickSort(int low, int high) {
+    if (low < high) {
+        int pi = partition(low, high);
+
+        //sortir bagian kiri dan kana secara terpisah
+        quickSort(low, pi - 1);
+        quickSort(pi + 1, high);
+    }
 }
